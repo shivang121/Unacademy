cc318431b68f1c3f9314bc0982d1d430c817c63c
Shivang Agarwal <shivang2006@gmail.com>
2020-04-19 18:01:09 +0530

    Added some more functionalities

--- app.js
+++ app.js
@@ -9,47 +9,85 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
 
+client.on("error", function(error) {
+    console.error(error);
+});
+
 //index page (Entry point)
 app.get('/', (req, res) => {
-    res.render("index", { reply: "Connection Established" });
+    res.render("index", { type: "0", reply: "Connection Established" });
 });
 
 //set in redis
 app.get('/set', (req, res) => {
-    client.set(req.query.key, req.query.value, function (err, reply) {
+    client.set(req.query.key, req.query.value, (err, reply) => {
         if (err) {
-            res.render("index", { reply: err });
+            res.render("index", { type: "0", reply: err });
         }
-        else{
-            res.render("index", { reply: "Set operation performed" });
+        else {
+            res.render("index", { type: "0", reply: "Set operation performed" });
         }
     });
 });
 
+// GET method
 app.get('/get', (req, res) => {
-    client.get(req.query.key, function (err, reply) {
+    client.get(req.query.key, (err, reply) => {
         if (err) {
-            res.render("index", { reply: err });
+            res.render("index", { type: "0", reply: err });
         }
-        else{
-            res.render("index", { reply: "Key: " + req.query.key + " Value: " + reply });
+        else {
+            if(reply == null)
+            res.render("index", { type: "0", reply: "Key does not exist" });
+            else
+            res.render("index", { type: "0", reply: "Key: " + req.query.key + " Value: " + reply });
         }
     });
 });
 
-app.get('/expire', (req, res) => {
-    client.expire(req.query.key, req.query.value, function (err, reply) {
+// delete key
+app.get("/delete", (req, res) => {
+    client.del(req.query.key, (err, reply) => {
         if (err) {
-            res.render("index", { reply: err });
+            console.log(err);
+        } else {
+            if (reply == 1) {
+                res.render("index", { type: "0", reply: "Key Deleted" });
+            } else {
+                res.render("index", { type: "0", reply: "Key DOES NOT exist in DB" });
+            }
+        }
+    });
+});
+
+app.get("/keys", (req, res) => {
+    client.keys(req.query.key, (err, reply) => {
+        if (err) {
+            console.log(err);
+        } else {
+                res.render("index", { type: "2", reply: reply });
+        }
+    });
+});
+
+
+
+// set expiry
+app.get('/expire', (req, res) => {
+    client.expire(req.query.key, req.query.value, (err, reply) => {
+        if (err) {
+            res.render("index", { type: "0", reply: err });
         }
         else {
             if (reply)
-                res.render("index", { reply: "Expiry set for key: " + req.query.key });
+                res.render("index", { type: "0", reply: "Expiry set for key: " + req.query.key });
             else
-                res.render("index", { reply: req.query.key + " does not exist!!!!" });
+                res.render("index", { type: "0", reply: "\"" + req.query.key + "\" does not exist!!!!" });
         }
     });
 });
+
+// check time to expire
 app.get("/remaining", (req, res) => {
 
     client.ttl(req.query.key, (err, reply) => {
@@ -58,18 +96,19 @@ app.get("/remaining", (req, res) => {
         }
         else {
             if (reply == -1) {
-                res.render("index", { reply: "Timeout Not assigned for this key" });
+                res.render("index", { type: "0", reply: "Timeout Not assigned for this key" });
             }
             else if (reply == -2) {
-                res.render("index", { reply: "Key does not exist!!!!" });
+                res.render("index", { type: "0", reply: "Key does not exist" });
             }
             else {
-                res.render("index", { reply: "Time Remaining: " + reply });
+                res.render("index", { type: "0", reply: "Time Remaining: " + reply });
             }
         }
     });
 });
 
+// Add value in sorted set
 app.get("/zadd", (req, res) => {
     client.zadd(req.query.name, req.query.score, req.query.value, (err, reply) => {
         if (err) {
@@ -77,15 +116,16 @@ app.get("/zadd", (req, res) => {
         }
         else {
             if (reply == 0) {
-                res.render("index", { reply: "Updated " + req.query.value + " score to " + req.query.score });
+                res.render("index", { type: "0", reply: "Updated \"" + req.query.value + "\" score to " + req.query.score });
             }
             else {
-                res.render("index", { reply: "Added " + req.query.value + " in set: " + req.query.name });
+                res.render("index", { type: "0", reply: "Added \"" + req.query.value + "\" in set: \"" + req.query.name + "\" " });
             }
         }
     });
 });
 
+// find rank in sorted set
 app.get("/zrank", (req, res) => {
     client.zrank(req.query.name, req.query.value, (err, reply) => {
         if (err) {
@@ -94,14 +134,16 @@ app.get("/zrank", (req, res) => {
         else {
             console.log(reply);
             if (reply != null) {
-                res.render("index", { reply: "0 based rank of " + req.query.value + " in set: " + req.query.name + " is " + reply });
+                res.render("index", { type: "0", reply: "0 based rank of \"" + req.query.value + "\" in set: \"" + req.query.name + "\" is " + reply });
             }
             else {
-                res.render("index", { reply: req.query.value + " does not exist in set: " + req.query.name });
+                res.render("index", { type: "0", reply: "\""+req.query.value + "\" does not exist in set: \"" + req.query.name + "\"" });
             }
         }
     });
 });
+
+//find all elements in given range in sorted set
 app.get("/zrange", (req, res) => {
     console.log(req.query);
     if (req.query.withscores == "true") {
@@ -111,11 +153,11 @@ app.get("/zrange", (req, res) => {
             }
             else {
                 if (reply.length != 0) {
-                    res.render("index", { reply: reply });
-                    // res.send(reply);
+                    console.log(reply);
+                    res.render("index", { reply: reply, type: "1" });
                 }
                 else {
-                    res.render("index", { reply: "Set "+ req.query.name + " doesn't contains any value" });
+                    res.render("index", { reply: "Set \"" + req.query.name + "\" doesn't contains any value", type: "0" });
                 }
             }
         });
@@ -127,28 +169,28 @@ app.get("/zrange", (req, res) => {
             }
             else {
                 if (reply.length != 0) {
-                    res.render("index", { reply: reply });
-                    // res.send(reply);
+                    res.render("index", { reply: reply, type: "2" });
                 }
                 else {
-                    res.render("index", { reply: "Set "+ req.query.name + " doesn't contains any value" });
+                    res.render("index", { reply: "Set \"" + req.query.name + "\" doesn't contains any value", type: "0" });
                 }
             }
         });
     }
 });
 
+// flush the existing DB
 app.get('/flush', (req, res) => {
     client.flushall((err, reply) => {
         if (err) {
-            res.render("index", { reply: err });
+            res.render("index", { type: "0", reply: err });
         }
-        else{
-            res.render("index", { reply: "Database Flushed" });
+        else {
+            res.render("index", { type: "0", reply: "Database Flushed" });
         }
     });
 });
 
-open("http://localhost:"+PORT);
+open("http://localhost:" + PORT);
 
 app.listen(PORT);
